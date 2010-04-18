fb8a550f0a0b3d583428c6ef635ebdb570961f41
winton <mail@wintoni.us>
2010-04-18 13:00:10 -0700

    Comments

--- lib/with_pid.rb
+++ lib/with_pid.rb
@@ -2,16 +2,23 @@ require 'rubygems'
 require 'erb'
 
 def with_pid(options)
+  # Add pid_dir option
   options[:pid_dir] = File.dirname(options[:pid])
+  
+  # Render bash script
   template = File.dirname(__FILE__) + '/../templates/bash.erb'
   template = File.expand_path(template)
   template = File.read(template)
   template = ERB.new(template).result(binding)
   bash_path = options[:tmp] + "/" + options[:name]
+  
+  # Write bash script to tmp
   FileUtils.mkdir_p(options[:tmp])
   File.open(bash_path, 'w') do |f|
     f.write(template)
   end
+  
+  # Execute bash script
   `chmod +x #{bash_path}`
   `#{bash_path}`
 end
